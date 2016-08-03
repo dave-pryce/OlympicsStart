fa9cd408e3f1901cf4862843886d39ffccc014ed
David Pryce <dpryce@seek.com.au>
2016-08-04 07:49:08 +1000

    end to end working.

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,8 @@
 import angular from 'angular'
 angular.module('olympics',[])
-.controller('sportsController', function(){
-  this.sports = ["WeightLifting", "Cycling","Running"]
+.controller('sportsController', function($http){
+  //this.sports = ["WeightLifting", "Cycling","Running"]
+  $http.get('/sports').then((response) => {
+    this.sports = response.data;
+  });
 })
