1aa34cea99a62772e7186227a312dc43f45015f6
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-08-03 19:59:27 +1000

    mongo connection added and returning data.

--- server/app.js
+++ server/app.js
@@ -3,12 +3,21 @@
 let express = require("express");
 let app = express();
 
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use(express.static(__dirname + "/../client") );
 
 
 
 app.get("/sports", (req, res) => {
-  res.json(["WeightLifting", "Cycling","Running"]);
+  let sports = mongoUtil.sports();
+  sports.find().toArray((err,docs) => {
+    console.log(JSON.stringify(docs));
+    let sportNames = docs.map((sport) => sport.name);
+    res.json(sportNames);
+  });
+  //res.json(["WeightLifting", "Cycling","Running","Athletics"]);
 });
 
 app.listen(8181, () => console.log("Listening on 8181"));
