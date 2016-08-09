7390f7a69b212810b5b4e5bcd069c9b41f288bfc
dave-pryce <dp@Davids-MacBook-Pro.local>
2016-08-09 16:55:55 +1000

    added angular-ui-router

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,17 @@
 import angular from 'angular'
-angular.module('olympics',[])
+import 'angular-ui-router'
+angular.module('olympics',["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider) => {
+  $urlRouterProvider.otherwise('/sports')
+
+  $stateProvider
+  .state('sports', {
+    url : '/sports',
+    templateUrl: '/sports/sports-nav.html',
+  })
+})
+
 .controller('sportsController', function($http){
   //this.sports = ["WeightLifting", "Cycling","Running"]
   $http.get('/sports').then((response) => {
