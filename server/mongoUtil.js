81b93bb051611b019289fdab96922f8532ff14ff
David Pryce <dpryce@seek.com.au>
2016-09-06 07:30:44 +1000

    update mongo connection to environment var

--- server/mongoUtil.js
+++ server/mongoUtil.js
@@ -6,7 +6,11 @@ let _db;
 
 module.exports = {
   connect() {
-    client.connect('mongodb://localhost:27017/olympics-dev', (err, db) =>{
+    //prod
+    client.connect(process.env.MONGODB_URI, (err, db) =>{
+
+    // dev
+    //client.connect('mongodb://localhost:27017/olympics-dev', (err, db) =>{
       if(err) {
         console.log("Error connecting to Mongo");
         process.exit(1);
